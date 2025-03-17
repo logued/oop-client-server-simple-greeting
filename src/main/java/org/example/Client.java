a98d0170b5419c0c1a23c040c456084e36e33c6f
logued <logued@dkit.ie>
2025-03-17 00:29:35 +0000

    variable name updates

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -5,36 +5,43 @@ import java.net.UnknownHostException;
 
 public class Client {
 
+    final static int PORT_NUMBER_ON_SERVER = 8888; // port number set in the server to listen for connection requests
+
     public static void main(String[] args) {
         Client client = new Client();
-        client.start(8888); // start client to connect to the port number that the server uses
+        client.start(); // start client
     }
 
-    public void start(int portNumber) {
+    public void start() {
 
-        try (   // create socket in client to connect to the server
-                Socket socket = new Socket("localhost", 8888);
-                // get the socket's output stream
-                OutputStream outputStream = socket.getOutputStream();
+        try (   // attempt to connect to the server (running on localhost) on the specified port number
+                // If the connection is successful, a new Socket will be created.
+                Socket socket = new Socket("localhost", PORT_NUMBER_ON_SERVER);
         ) {
-            PrintWriter out = new PrintWriter(outputStream, true);
+            // get the socket's output stream
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter socketWriter = new PrintWriter(outputStream, true);
             System.out.println("The client is running and has connected to the server.");
-            System.out.println("The client is sending teh message \"hello server\" to the server, and will await a response.");
+            System.out.println("The client is sending the message \"hello server\" to the server, and will await a response.");
 
-            out.println("hello server");    // send greeting message to server via socket
+            socketWriter.println("hello server");    // send greeting message to server via socket
 
-            // Next, deal with the response from the server:
-            // - get the input stream from the socket, and wrap in a reader
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // Next, wait for response from the server, and when it arrives, deal with it.
 
-            String response = in.readLine();    // wait, and receive a return message from the server
+            // - get the input stream from the socket, and wrap in a Reader
+            BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String response = socketReader.readLine();    // wait, and receive a return message from the server
+            // execution of this code stops at readline(), which waits for input from teh server.
+            // The input stream of characters MUST be terminated by a newline ( \n )
+
             System.out.println("In client: The server response was : " + response);
             System.out.println("Finished! - client is exiting.");
 
         } catch (UnknownHostException e) {
-            System.out.println(e);  // print out the exception
+            System.out.println(e.toString());  // print out the exception
         } catch (IOException e) {
-            System.out.println(e);
+            System.out.println(e.toString());
         }
     }
 }
--- src/main/java/org/example/GreetingServer.java
+++ src/main/java/org/example/GreetingServer.java
@@ -1,20 +1,21 @@
-package org.example;
+package org.example;                // March 2025
 /**
- * A basic Client - Server program that demonstrates how a Server can
- * create a ServerSocket that allows a client to connect.
- * Once connected, the client sends a greeting message to the server,
- * and the server responds by sending a reply to the client.
- * All messages travel via the socket.
- *
+ * A basic Client/Server program that demonstrates how a Server
+ * creates a ServerSocket that allows a client to connect to it.
+ * Once connected, and the Socket is established, the client sends
+ * a greeting message to the server and the server responds by sending a reply to the client.
+ * All messages travel via the Socket.
+ * <p>
  * Note in particular the use of the .accept() and .readLine() methods which
- * are Blocking IO methods.  This means that they pause the execution of the
+ * are "Blocking IO" methods.  This means that they pause the execution of the
  * program at this point, and wait for something to happen before proceeding.
  * (e.g. wait for a client to connect, or wait for a message to arrive
  * (followed by a '\n' character.)
- *
+ * <p>
  * Run the GreetingServer first, and then run the Client.
  * Study the two output panes (GreetingServer and Client) for results.
  */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -24,7 +25,7 @@ import java.net.Socket;
 
 public class GreetingServer {
 
-    final int PORT_NUMBER = 8888;  // could be any port from 1024 to 49151 (that doesn't clash with other Apps)
+    final static int PORT_NUMBER = 8888;  // could be any port from 1024 to 49151 (that doesn't clash with other Apps)
 
     public static void main(String[] args) {
         GreetingServer server = new GreetingServer();
@@ -40,31 +41,33 @@ public class GreetingServer {
 
         try (ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
              Socket clientSocket = serverSocket.accept();       // start server to listen on port number 8888
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             PrintWriter socketWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader socketReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         ) {
+
+
             System.out.println("The GreetingServer server has started and is waiting for a client to connect.");
 
-            String message = in.readLine(); // read the incoming message - terminated by a newline ("\n")
+            String message = socketReader.readLine(); // read the incoming message - terminated by a newline ("\n")
             // program stops and waits here until input (followed by '\n') arrives.
 
-            System.out.println("The GreetingServer has received this message from a client: " + message);
-            System.out.println("The server is replying to the client.");
+            System.out.println("The GreetingServer has received the following message from a client: " + message);
+            System.out.println("The server will now replying to the client.");
 
             if ("hello server".equals(message)) {
-                out.println("hello client");    // send response back to client
+                socketWriter.println("hello client");    // send response back to client
             } else {
-                out.println("unrecognised greeting");
+                socketWriter.println("unrecognised greeting");
             }
 
             System.out.println("The GreetingServer is finished, and is exiting. Goodbye!");
 
         } catch (IOException exception) {
-            System.out.println(exception);
+            System.out.println(exception.toString());
         }
         // the try-with-resources style of exception handling used above is preferred
         // as it will automatically close all AutoClosable resources such as
-        // the server socket and streams.
+        // the server socket and the streams.
     }
 
     //TODO Exercise
